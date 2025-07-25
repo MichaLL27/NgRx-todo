f747a2832934008822fab437b3bd804dfabeefb4
MichaLL27 <lazarashvili2704@gmail.com>
2025-07-25 17:04:23 +0400

    redux devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,16 +6,22 @@ import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { todosReducer } from './todos/store/reducers/todos.reducer';
 import { TodoEffects } from './todos/store/effects/todo.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 export const appConfig = {
   providers: [
     importProvidersFrom(
       HttpClientModule,
-      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 500 })
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 500 }),
+      StoreDevtoolsModule.instrument({
+        // install Reducer DevTools Extension: Redux DevTools Chrome Extension to see the state changes
+        maxAge: 25,        
+        logOnly: false,       
+      })
     ),
     provideStore({
       todos: todosReducer,
     }),
-    provideEffects([TodoEffects]), 
+    provideEffects([TodoEffects]),
   ]
 };
