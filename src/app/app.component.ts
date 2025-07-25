8598c6a60554f8c03ff67e7b175ca04bb3613e4d
MichaLL27 <lazarashvili2704@gmail.com>
2025-07-25 15:12:26 +0400

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './todos/components/todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TodosComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
